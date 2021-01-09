df05c6fc3d2b8346ad59a6737b5b32ae6f190ca3
Oshimani <jannick2.0@googlemail.com>
2021-01-09 14:55:54 +0100

    added properties to poems

--- src/Entity/Poem.php
+++ src/Entity/Poem.php
@@ -17,8 +17,76 @@ class Poem
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $praise;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $created;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPraise(): ?int
+    {
+        return $this->praise;
+    }
+
+    public function setPraise(int $praise): self
+    {
+        $this->praise = $praise;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(?\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
     }
 }
